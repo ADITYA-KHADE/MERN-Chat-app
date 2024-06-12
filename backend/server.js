b999aa69144ab484e237e5b10cd2d3d523dab6b4
adityakhade321 <adityakhadeak47@gmail.com>
2024-06-13 01:10:35 +0530

    comit

--- backend/server.js
+++ backend/server.js
@@ -2,38 +2,43 @@ const express = require("express");
 // const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
-const cookieParser=require("cookie-parser")
+const cookieParser = require("cookie-parser");
+const { app, server } = require("./sockets.io/socket");
+
+const path = require("path");
+// const __dirname = path.resolve();
 
 dotenv.config();
-const PORT = process.env.PORT || 5000 ;
+const PORT = process.env.PORT || 5000;
 
-const mongodb=require("./config/connectdb");
+const mongodb = require("./config/connectdb");
 mongodb();
-
-const{ app, server } = require("./sockets.io/socket");
 
 app.use(cors());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
+const frontendPath = path.resolve(__dirname, "../frontend/dist");
+app.use(express.static(frontendPath));
 
-const authRoute=require("./routes/authRoutes");
-const messageRoute=require("./routes/messageRoutes");
-const userRoute=require("./routes/userRoutes");
+// Logging to check paths
+// console.log("Static files served from:", frontendPath);
+// console.log("Index file path:", path.resolve(frontendPath, "index.html"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(frontendPath, "index.html"));
+});
+
+// routes
+const authRoute = require("./routes/authRoutes");
+const messageRoute = require("./routes/messageRoutes");
+const userRoute = require("./routes/userRoutes");
 
 app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
-app.use("/api/users",userRoute);
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-    }
-);
-
+app.use("/api/users", userRoute);
 
 server.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
-    }
-);
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
