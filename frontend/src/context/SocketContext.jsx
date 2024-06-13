48a40db9d753ef414faf0642d3fb50731123f097
adityakhade321 <adityakhadeak47@gmail.com>
2024-06-13 17:15:49 +0530

    commit

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({children}) => {
   
     useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:8000", {
+			const socket = io("https://mern-chat-app-9alu.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
